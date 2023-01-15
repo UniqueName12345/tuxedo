ef00ec8eb26ed6c2aae1e8878b1c24366da65d38
jdev082 <willsjayden2@gmail.com>
2023-01-15 10:12:16 -0600

    feat: xD

---
+++ source-code.py
@@ -0,0 +1,44 @@
+# Source Generated with Decompyle++
+# File: SUIT.pyc (Python 3.10)
+
+import os
+import json
+import requests
+print('SUIT Client v1.0 (CC0 software): A way to use SUI without the trouble!\xe2\x84\xa2')
+if not os.path.exists('suit-cache.json'):
+    print("It appears that the cache file (suit-cache.json) doesn't exist. To prevent crashing, a new one will be generated. \n")
+    cachefile = open('suit-cache.json', 'w')
+    cachefile.write(json.dumps({
+        'pppdud': 'is the best' }))
+    cachefile.close()
+    print('Cache file regenerated. \n')
+print('Loading cache..')
+cachefile = open('suit-cache.json', 'r')
+print('Extracting JSON data..')
+cachefile = json.loads(cachefile.read())
+username = input('Please type in a username or ID here: ')
+cache_file_data = open('suit-cache.json', 'w')
+
+try:
+    if username in cachefile:
+        print('Username found in cache. Loading data..')
+        print(cachefile[username])
+    elif json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_id/{username}''').text)['Error'] == False:
+        print('Info available online. Downloading data..')
+        cachefile[json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_id/{username}''').text)['Username']] = json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_id/{username}''').text)['ID']
+        print(cachefile[username])
+    elif json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_user/{username}''').text)['Error'] == False and username not in list(cachefile.values()):
+        print('ID available online but not downloaded. Downloading ID..')
+        print(json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_user/{username}''').text)['Username'])
+        cachefile[json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_user/{username}''').text)['Username']] = json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_user/{username}''').text)['ID']
+    elif json.loads(requests.get(f'''https://sui.sid72020123.repl.co/get_user/{username}''').text)['Error'] == False and username in list(cachefile.values()):
+        print('ID stored in cache. Loading data..')
+        print(list(cachefile.keys())[list(cachefile.values()).index(username)])
+    else:
+        print('Username or ID not found.')
+finally:
+    pass
+cache_file_data.write(json.dumps(cachefile))
+cache_file_data.close()
+continue
+
